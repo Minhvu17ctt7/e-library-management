36969cf846bd577424b0da70f23fa203873f6c63
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2021-11-25 15:15:45 +0700

    second commit

--- config/database.js
+++ config/database.js
@@ -1,15 +1,43 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  if (env('NODE_ENV') ==='production') {
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "mysql",
+            host,
+            port,
+            database,
+            username: user,
+            password,
+            ssl: { rejectUnauthorized: false },
+          },
+          options: {
+            ssl: false,
+          },
+        },
       },
-      options: {
-        useNullAsDefault: true,
+    };
+  }
+
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'sqlite',
+          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        },
+        options: {
+          useNullAsDefault: true,
+        },
       },
     },
-  },
-});
+  }
+};
